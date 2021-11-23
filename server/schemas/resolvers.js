7abb18737c0135ed30d748345909c589bae40c00
Philip Tantsidis <ptantsidis@gmail.com>
2021-11-23 14:12:45 -0500

    habit typedef and resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,13 +1,45 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Thought } = require('../models');
+const { User, Habit, LifeStyle } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-
+    habits: async (parent, { lifeStyle }) => {
+      const params = lifeStyle ? { lifeStyle } : {};
+      return Habit.find(params).sort({ createdAt: -1 });
+    },
+    habit: async (parent, { habitId }) => {
+      return Habit.findOne({ _id: habitId });
+    },
   },
-
   Mutation: {
-  }
+    addHabit: async (parent, { habitName }, context) => {
+      if (context.user) {
+        const habit = await Habit.create({
+          habitName,
+          timeLine,
+          quantity,
+        });
+        return habit;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
+  removeHabit: async (parent, { habitId }, context) => {
+    if (context.user) {
+      const habit = await Habit.findOneAndDelete({
+        _id: habitId,
+      });
+
+      await User.findOneAndUpdate(
+        { _id: context.user._id },
+        { $pull: { habits: habit._id } }
+      );
+
+      return habit;
+    }
+    throw new AuthenticationError('You need to be logged in!');
+  },
 }
+
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,20 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  
+type habit {
+    habitId: String!
+    habitName: String!
+    timeLine: String!
+    quantity: Int!
+  },
+type query {
+    habits(lifeStyle: String): [Habit]
+    habit(habitId: ID!): Habit
+    }
+}
+  type Mutation {
+    addHabit(input: addHabitInput): lifeStyle
+    removeHabit(habitId: String!): lifeStyle
 `;
 
 module.exports = typeDefs;
