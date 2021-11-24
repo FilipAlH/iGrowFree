6dec8d6379ec3bcdf9c2ba900b571f90dd3155b1
unknown <filip.a.11@hotmail.com>
2021-11-24 14:31:46 -0500

    fixed queries for lifestyle

--- server/models/Lifestyle.js
+++ server/models/Lifestyle.js
@@ -2,9 +2,8 @@ const { Schema, model } = require('mongoose');
 
 const LifeStyleSchema = new Schema(
     {
-        LifeStyleType: {
+        lifeStyleType: {
             type: String,
-            required: true,
 
         },
 
