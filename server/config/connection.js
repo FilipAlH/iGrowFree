251172887463a0170bb2809c3ba98bd51dfd18a1
romzz.git <romzz.patel@gmail.com>
2021-11-30 02:49:54 -0500

    final deployment1

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb+srv://romzz-patel:vrz126117@cluster0.pwd2e.mongodb.net/iGrowFree_project3?retryWrites=true&w=majority',
+  process.env.MONGODB_URI || 'mongodb://localhost/iGrowFree_project3',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
