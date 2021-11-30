499070660f5445fcaaf1b4f8f1b146bf1de35e28
romzz.git <romzz.patel@gmail.com>
2021-11-30 01:03:10 -0500

    deploy try2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  purge: {
-    enabled: true,
-    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  },
+  purge: [
+    './src/**/*.html',
+    './src/**/*.js',
+    './src/**/*.jsx',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
